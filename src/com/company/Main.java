6503248c6944f00cf474279f0d34cae7f5c6a8ae
Alex Mac <astalter@bridgew.edu>
2019-11-14 16:16:15 -0500

    Made program window fill entire screen
    used toolkit to get screen size of the current device

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,17 +1,12 @@
 package com.company;
 
-import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
         CreateWindow main = new CreateWindow();
         main.loginWindow();
-        if(Arrays.toString(main.fieldPassword.getPassword()).equals("hello")){
-
-            System.out.println("correct password");
-
-        }
+        
 
 
     }
