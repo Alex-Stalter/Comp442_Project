4c095f715675ca7466ce0983b72caf116e85a1a3
Alex Mac <astalter@bridgew.edu>
2019-11-14 16:01:36 -0500

    Added limited functionality to CreateWindow

--- src/com/company/CreateWindow.java
+++ src/com/company/CreateWindow.java
@@ -3,46 +3,51 @@ package com.company;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.util.Arrays;
 
-public class CreateWindow extends JFrame{
+public class CreateWindow extends JFrame implements ActionListener{
     private JLabel labelUsername = new JLabel("Enter username: ");
     private JLabel labelPassword = new JLabel("Enter password: ");
     private JTextField textUsername = new JTextField(20);
     public JPasswordField fieldPassword = new JPasswordField(20);
     private JButton buttonLogin = new JButton("Login");
+    private JPanel loginPane = new JPanel(new GridBagLayout());
     public void loginWindow(){
 
     //Creating login screen for management system
 
 
 
-        JPanel pane = new JPanel(new GridBagLayout());
+
         GridBagConstraints constraints = new GridBagConstraints();
         constraints.insets = new Insets(10,10,10,10);
         constraints.gridx = 0;
         constraints.gridy = 0;
-        pane.add(labelUsername, constraints);
+        loginPane.add(labelUsername, constraints);
 
         constraints.gridx = 1;
-        pane.add(textUsername, constraints);
+        loginPane.add(textUsername, constraints);
 
         constraints.gridx = 0;
         constraints.gridy = 1;
-        pane.add(labelPassword, constraints);
+        loginPane.add(labelPassword, constraints);
 
         constraints.gridx = 1;
-        pane.add(fieldPassword, constraints);
+        loginPane.add(fieldPassword, constraints);
 
         constraints.gridx = 0;
         constraints.gridy = 2;
         constraints.gridwidth = 2;
         constraints.anchor = GridBagConstraints.CENTER;
-        pane.add(buttonLogin, constraints);
+        loginPane.add(buttonLogin, constraints);
 
-        pane.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), "Login Panel"));
+        buttonLogin.addActionListener(this);
+
+
+        loginPane.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), "Login Panel"));
         setSize(new Dimension(500,500));
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        add(pane);
+        add(loginPane);
         setVisible(true);
 
 
@@ -50,4 +55,20 @@ public class CreateWindow extends JFrame{
 
     }
 
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        StringBuilder password= new StringBuilder();
+        for(int i =0;i<fieldPassword.getPassword().length;i++){
+            password.append(fieldPassword.getPassword()[i]);
+        }
+        if(textUsername.getText().equals("Manager")&&password.toString().equals("manager")){
+            loginPane.removeAll();
+            JLabel welcome = new JLabel("Welcome Manager!");
+            loginPane.add(welcome);
+            revalidate();
+            repaint();
+        }
+
+    }
 }
