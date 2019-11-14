87d3194b2356b6fd3cef8b83855038ce627bb15d
Alex <astalter@bridgew.edu>
2019-11-14 14:55:08 -0500

    Created simple login page

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,18 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
         CreateWindow main = new CreateWindow();
-        main.initializeWindow();
+        main.loginWindow();
+        if(Arrays.toString(main.fieldPassword.getPassword()).equals("hello")){
+
+            System.out.println("correct password");
+
+        }
+
 
     }
 }
