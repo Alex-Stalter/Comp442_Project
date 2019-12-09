744217e5c523602ce59a796f5005edd924b40bde
Alex Mac <astalter@bridgew.edu>
2019-12-08 23:24:24 -0500

    Added more functionality to owner class
    Added weekly cost income and net income as well as ratios for managers cleaners and maintenance

--- src/com/company/Owner.java
+++ src/com/company/Owner.java
@@ -1,21 +1,25 @@
 package com.company;
 
-import java.util.Arrays;
 
 public class Owner {
     public String password = "Owner";
     public String username = "owner";
-    public Manager[] managers = new Manager[4];
+    public int maxManager = 4;
+    public int maxMaint = 6;
+    public int maxCleaner = 8;
+    public int maxCustomer = 25;
+    public Manager[] managers = new Manager[maxManager];
     public int managerNumber = 0;
-    public Maint[] maint = new Maint[6];
+    public Maint[] maint = new Maint[maxMaint];
     public int maintNumber = 0;
-    public Cleaner[] cleaner = new Cleaner[8];
+    public Cleaner[] cleaner = new Cleaner[maxCleaner];
     public int cleanerNumber = 0;
-    public Customer[] customer = new Customer[25];
+    public Customer[] customer = new Customer[maxCustomer];
     public int customerNumber = 0;
     public Room[] hotelRooms = new Room[25];
-    public Room[] bookedRooms = new Room[25];
     public int roomValueMult = 1;
+    public int cost = 0;
+    public int income = 0;
 
     Owner(){
         createRooms();
@@ -31,6 +35,8 @@ public class Owner {
         Cleaner cleaner01 = new Cleaner();
         cleanerNumber++;
         cleaner[0]=cleaner01;
+        calculateCost();
+        calculateIncome();
 
     }
 
@@ -60,6 +66,68 @@ public class Owner {
 
         }
 
+    }
+    public void hireMaint(){
+        if(maintNumber<maxMaint){
+            Maint maintainer = new Maint();
+            maint[maintNumber] = maintainer;
+            maintNumber++;
+        }
+        calculateCost();
+    }
+    public void hireManager(){
+        if(managerNumber<maxManager){
+            Manager newManager = new Manager();
+            managers[managerNumber] = newManager;
+            managerNumber++;
+        }
+        calculateCost();
+    }
+    public void hireCleaner(){
+        if(cleanerNumber<maxCleaner){
+            Cleaner newCleaner = new Cleaner();
+            cleaner[cleanerNumber] = newCleaner;
+            cleanerNumber++;
+
+        }
+        calculateCost();
+    }
+    public void fireMaint(){
+        if(maintNumber>0){
+            maint[maintNumber-1] = null;
+            maintNumber--;
+        }
+        calculateCost();
+
+
+    }
+    public void fireManager(){
+        if(managerNumber>0){
+            managers[managerNumber-1] = null;
+            managerNumber--;
+        }
+        calculateCost();
+
+    }
+    public void fireCleaner(){
+        if(cleanerNumber>0){
+            cleaner[cleanerNumber-1] = null;
+            cleanerNumber--;
+        }
+        calculateCost();
+    }
+    public void calculateCost(){
+        cost = 150*maintNumber + 300*managerNumber + 150*cleanerNumber;
+    }
+    public void calculateIncome(){
+
+        for (Room booked: hotelRooms) {
+
+            if(booked.booked){
+                income+=booked.roomValue;
+            }
+
+        }
     }
 
 }
--- src/com/company/Room.java
+++ src/com/company/Room.java
@@ -4,4 +4,5 @@ public class Room {
     public int roomNumber = 0;
     public int roomValue = 0;
     public String roomDesc = "";
+    public Boolean booked = false;
 }
