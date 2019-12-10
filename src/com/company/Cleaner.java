7afa1e17a8be087d7684445823683e4167dd9912
Alex Mac <astalter@bridgew.edu>
2019-12-10 09:52:09 -0500

    Comments added
    comments added to not implemented classes that are planned to be implemented

--- src/com/company/Cleaner.java
+++ src/com/company/Cleaner.java
@@ -1,4 +1,5 @@
 package com.company;
 
 public class Cleaner {
+    //cleaner class not implemented
 }
--- src/com/company/Maint.java
+++ src/com/company/Maint.java
@@ -1,4 +1,5 @@
 package com.company;
 
 public class Maint {
+    //maint class nt implemented
 }
